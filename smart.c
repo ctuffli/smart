be76fda87022cc1732dc03b06c20273acf174e7a
Chuck Tuffli <chuck@tuffli.net>
2017-08-10 12:01:53 -0700

    Remove typo

--- smart.c
+++ smart.c
@@ -13,8 +13,7 @@
  * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
-/*
- */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <getopt.h>
