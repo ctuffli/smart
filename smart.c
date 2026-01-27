4458fe1c7611cccdce7271e598c327248a6e50b6
Chuck Tuffli <chuck@tuffli.net>
2026-01-27 12:15:17 -0800

    fix: JSON output for ATA threshold attributes
    The ATA threshold output when using libxo created malformed JSON
    
    fixes #10

--- smart.c
+++ smart.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2021 Chuck Tuffli <chuck@tuffli.net>
+ * Copyright (c) 2016-2026 Chuck Tuffli <chuck@tuffli.net>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -26,7 +26,7 @@
 #include "libsmart.h"
 
 #define SMART_NAME "smart"
-#define SMART_VERSION	"0.4.2"
+#define SMART_VERSION	"1.0.0"
 
 const char *pn;
 bool do_debug = false;
@@ -266,7 +266,7 @@ main(int argc, char *argv[])
 
 	if (do_version) {
 		printf("%s, version %s\n", pn, SMART_VERSION);
-		printf("Copyright (c) 2016-2025 Chuck Tuffli\n"
+		printf("Copyright (c) 2016-2026 Chuck Tuffli\n"
 				"This is free software; see the source for copying conditions.\n");
 		return EXIT_SUCCESS;
 	}
